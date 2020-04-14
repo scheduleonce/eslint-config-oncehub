be330f78f4a25e78e7dffc8ea0adba907a865e7c
rajat saxena <rajat.saxena@oncehub.com>
2020-04-14 16:18:11 +0530

    Linting rules for OnceHub

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
     'node': true,
   },
   'extends': [
-    'google',
+    'plugin:@typescript-eslint/recommended',
     'plugin:sonarjs/recommended',
     'prettier'
   ],
@@ -20,5 +20,28 @@ module.exports = {
   'plugins': [
     '@typescript-eslint',
     'sonarjs'
-  ]
+  ],
+  'overrides': [
+    {
+      'files': ['**/*(specs|tests)/**', '**/*.spec.ts'],
+      'rules': {
+        '@typescript-eslint/no-empty-function': 'off',
+        'sonarjs/no-identical-functions': 'off',
+        'sonarjs/no-duplicate-string': 'off'
+      }
+    }
+  ],
+  'rules': {
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/consistent-type-assertions': ['error', {
+      assertionStyle: 'angle-bracket'
+    }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
 };
