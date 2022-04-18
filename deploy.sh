33ece6386793f635e5e8b070c62a9f56da248765
so-hvats <hvats@oncehub.com>
2022-04-18 18:13:49 +0530

    https://scheduleonce.atlassian.net/browse/ONCEHUB-53279

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,13 @@ npm i
 npm run package
 filename="$(npm pack --dry-run | tail -n 1)"
 npm publish $filename --registry=https://registry.npmjs.org/
+echo "$1"
+
+if [[ "$1" == "qa" ]] || [[ "$1" == "master" ]] || [[ "$1" == "staging" ]] || [[ "$1" == "staging-app2" ]]
+then
+  npm publish $filename --registry=https://registry.npmjs.org/
+else
+  npm publish --tag beta $filename --registry=https://registry.npmjs.org/
+fi
 
 echo "$filename package pushed to NPM successfully"
